2a5120f8bebdf068d6be978c7a0ea6f0d27a1bf6
danielxiong <ewall1106@gmail.com>
2025-02-03 21:52:43 +0800

    feat: 开机的时候清空input/output/user

--- main.py
+++ main.py
@@ -236,6 +236,23 @@ def cleanup_temp():
         shutil.rmtree(temp_dir, ignore_errors=True)
 
 
+def cleanup_user():
+    temp_dir = folder_paths.get_user_directory()
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir, ignore_errors=True)   
+
+
+def cleanup_input():
+    temp_dir = folder_paths.get_input_directory()
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir, ignore_errors=True)
+  
+def cleanup_output():
+    temp_dir = folder_paths.get_output_directory()
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir, ignore_errors=True)
+        
+            
 def start_comfyui(asyncio_loop=None):
     """
     Starts the ComfyUI server using the provided asyncio event loop or creates a new one.
@@ -245,7 +262,11 @@ def start_comfyui(asyncio_loop=None):
         temp_dir = os.path.join(os.path.abspath(args.temp_directory), "temp")
         logging.info(f"Setting temp directory to: {temp_dir}")
         folder_paths.set_temp_directory(temp_dir)
+        
     cleanup_temp()
+    cleanup_user()
+    cleanup_input()
+    cleanup_output()
 
     if args.windows_standalone_build:
         try:
@@ -302,3 +323,6 @@ if __name__ == "__main__":
         logging.info("\nStopped server")
 
     cleanup_temp()
+    cleanup_user()
+    cleanup_input()
+    cleanup_output()
